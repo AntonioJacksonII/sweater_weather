645bbd6b158891bd52e56dbe4de8d08b8410e248
Antonio Jackson <antoniojackson@gmail.com>
2020-08-03 07:48:13 -0600

    Updating action name to create for the forecast route

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,5 +1,11 @@
 class Api::V1::ForecastController < ApplicationController
 
-  def show
+  def create
+    location = params[:location]
+    search_results = SearchResults.new(location)
+    coordinates = search_results.lat_long
+    forecast_results = search_results.open_weather_forecast(coordinates)
+    @forecast = Forecast.new(location, forecast_results)
+    render json: ForecastSerializer.new(@forecast)
   end
 end

8dcbb156a1e6f1f82a55a0fd7b619890f8494af9
Antonio Jackson <antoniojackson@gmail.com>
2020-08-03 07:45:53 -0600

    Adding search_results facade

---
+++ app/facades/search_results.rb
@@ -0,0 +1,18 @@
+class SearchResults
+  attr_reader :location
+
+  def initialize(location)
+    @location = location
+  end
+
+  def lat_long
+    json = MapquestService.new.get_lat_long(location)
+    {name: location,
+     latitude: json[:results].first[:locations].first[:latLng][:lat],
+     longitude: json[:results].first[:locations].first[:latLng][:lng]}
+  end
+
+  def open_weather_forecast(coordinates)
+    OpenweatherService.new.get_forecast(coordinates)
+  end
+end

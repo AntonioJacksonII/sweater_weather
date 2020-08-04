d344a951d735cb96dc43c2d2aee5fac0a7aef9cc
Antonio Jackson <antoniojackson@gmail.com>
2020-08-04 16:30:45 -0600

    Updating forecast to a local variable

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -5,8 +5,8 @@ class Api::V1::ForecastController < ApplicationController
     search_results = SearchResults.new(location)
     coordinates = search_results.lat_long
     forecast_results = search_results.open_weather_forecast(coordinates)
-    @forecast = Forecast.new(location, forecast_results)
-    render json: ForecastSerializer.new(@forecast)
+    forecast = Forecast.new(location, forecast_results)
+    render json: ForecastSerializer.new(forecast)
   end
 
   private

1ccda4dbaaf68f3c24c56e46db5725a097a7c30b
Antonio Jackson <antoniojackson@gmail.com>
2020-08-04 09:32:30 -0600

    Updating trail request spec to be more robust and test for presence and absence of keys in the response and ensure the correct number of attributes/keys are returned

--- spec/requests/api/v1/trails_request_spec.rb
+++ spec/requests/api/v1/trails_request_spec.rb
@@ -10,12 +10,35 @@ describe "Trails API" do
     response = json[:data]
 
     expect(response[:type]).to eq('trail')
+    expect(response[:attributes]).to have_key(:location)
     expect(response[:attributes][:location]).to eq('denver,co')
+    expect(response[:attributes]).to have_key(:forecast)
     expect(response[:attributes][:forecast]).to_not be_empty
+    expect(response[:attributes][:forecast]).to_not have_key(:location)
+    expect(response[:attributes][:forecast]).to_not have_key(:latitude)
+    expect(response[:attributes][:forecast]).to_not have_key(:longitude)
+    expect(response[:attributes][:forecast]).to_not have_key(:hourly_forecast)
+    expect(response[:attributes][:forecast]).to_not have_key(:daily_forecast)
+    expect(response[:attributes][:forecast].keys.count).to eq(2)
+    expect(response[:attributes][:forecast]).to have_key(:summary)
     expect(response[:attributes][:forecast][:summary]).to_not be_empty
+    expect(response[:attributes][:forecast]).to have_key(:temperature)
     expect(response[:attributes][:forecast][:temperature]).to be_a Integer
 
     expect(response[:attributes][:trails]).to be_a Array
+    expect(response[:attributes][:trails].first).to be_a Hash
+    expect(response[:attributes][:trails].first.keys.count).to eq(5)
+    expect(response[:attributes][:trails].first).to_not have_key(:type)
+    expect(response[:attributes][:trails].first).to_not have_key(:stars)
+    expect(response[:attributes][:trails].first).to_not have_key(:starVotes)
+    expect(response[:attributes][:trails].first).to_not have_key(:url)
+    expect(response[:attributes][:trails].first).to_not have_key(:conditionDate)
+    expect(response[:attributes][:trails].first).to_not have_key(:ascent)
+    expect(response[:attributes][:trails].first).to have_key(:name)
+    expect(response[:attributes][:trails].first).to have_key(:summary)
+    expect(response[:attributes][:trails].first).to have_key(:difficulty)
+    expect(response[:attributes][:trails].first).to have_key(:location)
+    expect(response[:attributes][:trails].first).to have_key(:distance_to_trail)
     expect(response[:attributes][:trails].first[:name]).to_not be_empty
     expect(response[:attributes][:trails].first[:summary]).to_not be_empty
     expect(response[:attributes][:trails].first[:difficulty]).to_not be_empty

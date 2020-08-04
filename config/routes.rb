f0797edfd69fb07e16e8ecf1f23ff87048975ba6
Antonio Jackson <antoniojackson@gmail.com>
2020-08-04 11:38:54 -0600

    Updating forecast route to utilize index resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get '/forecast', to: 'forecast#create'
+      resources :forecast, only: [:index]
     end
   end
 end

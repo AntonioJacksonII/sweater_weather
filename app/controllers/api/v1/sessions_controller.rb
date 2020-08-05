b9e7c793d7bb02552fc55edbc00f9ba40e877251
Antonio Jackson <antoniojackson@gmail.com>
2020-08-04 23:39:11 -0600

    Updating sessions create action to handle incorrect passwords and unrecognized email addresses

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,13 +1,14 @@
 class Api::V1::SessionsController < ApplicationController
 
   def create
-    unless user = User.find_by(email: user_params[:email])
-      render json: "The entered email address does not identify a user", status: 400
-    end
+    user = User.find_by(email: user_params[:email])
     if user && user.authenticate(user_params[:password])
       session[:user_id] = user.id
       render json: UsersSerializer.new(user)
+    elsif user
+      render json: "The entered password is incorrect for the entered email address", status: 400
     else
+      render json: "The entered email address does not identify a user", status: 400
     end
   end
 

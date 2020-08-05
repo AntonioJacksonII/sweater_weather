87f2d65e59387964a2cc6463b2bf0e7401616bf1
Antonio Jackson <antoniojackson@gmail.com>
2020-08-04 23:12:12 -0600

    Adding the sessions create action and the current_user method

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::SessionsController < ApplicationController
+
+  def create
+    user = User.find_by(email: user_params[:email])
+    if user.authenticate(user_params[:password])
+      session[:user_id] = user.id
+      render json: UsersSerializer.new(user)
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password)
+  end
+end

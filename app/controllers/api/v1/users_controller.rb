ac9b6c0fdac7baf2a7e0c3e99349a287c1ab0530
Antonio Jackson <antoniojackson@gmail.com>
2020-08-04 21:56:32 -0600

    Updating create action in the users controller to display 400 status and error message when a new user isn't created

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,8 +3,12 @@ class Api::V1::UsersController < ApplicationController
   def create
     create_params = user_params.to_h
     create_params[:api_key] = SecureRandom.uuid
-    user = User.create!(create_params)
-    render json: UsersSerializer.new(user), status: :created
+    user = User.new(create_params)
+    if user.save
+      render json: UsersSerializer.new(user), status: :created
+    else
+      render json: user.errors.full_messages.first, status: 400
+    end
   end
 
   private

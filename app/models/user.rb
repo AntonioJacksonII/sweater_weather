d7c9de59a0ac4d5cfa0dac3334d8f2c96a443ef5
Antonio Jackson <antoniojackson@gmail.com>
2020-08-04 20:42:08 -0600

    Adding api_key to the users table with associated test for presence and uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   validates :password, presence: true
   validates :password_confirmation, presence: true
+  validates :api_key, uniqueness: true, presence: true
   has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,5 +6,7 @@ describe User, type: :model do
     it {should validate_uniqueness_of(:email)}
     it {should validate_presence_of(:password)}
     it {should validate_presence_of(:password_confirmation)}
+    it {should validate_presence_of(:api_key)}
+    it {should validate_uniqueness_of(:api_key)}
   end
 end

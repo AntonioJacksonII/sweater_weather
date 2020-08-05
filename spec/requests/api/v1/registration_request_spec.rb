f18058f1165e907d1cbe5f7d350a25db61cfa4b1
Antonio Jackson <antoniojackson@gmail.com>
2020-08-04 20:05:08 -0600

    Adding test for User Registration API

---
+++ spec/requests/api/v1/registration_request_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe "User Registration API" do
+  it "creates a user and generates a unique api key for that user" do
+    user_params = {"email": "whatever@example.com",
+                   "password": "password",
+                   "password_confirmation": "password"}
+    post "/api/v1/users", params: user_params
+
+    expect(response).to be_successful
+
+    json = JSON.parse(response.body, symbolize_names: true)
+    user = json[:data]
+
+    expect(user[:type]).to eq('users')
+    expect(user).to have_key(:id)
+    expect(user[:id]).to_not be_empty
+    expect(user[:attributes].keys.count).to eq(2)
+    expect(user[:attributes]).to have_key(:email)
+    expect(user[:attributes][:email]).to_not be_empty
+    expect(user[:attributes]).to have_key(:api_key)
+    expect(user[:attributes][:api_key]).to_not be_empty
+  end
+end

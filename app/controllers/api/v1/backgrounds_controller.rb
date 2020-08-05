fe1e108c1b5989d99bd96b27f310ccfa20c52459
Antonio Jackson <antoniojackson@gmail.com>
2020-08-05 10:52:17 -0600

    Updating BackgroundsController to pass in location to search_results.weather_background_image

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -2,8 +2,8 @@ class Api::V1::BackgroundsController < ApplicationController
 
   def index
     location = backgrounds_params[:location]
-    search_results = SearchResults.new(location)
-    background_results = search_results.weather_background_image
+    search_results = SearchResults.new
+    background_results = search_results.weather_background_image(location)
     background = Background.new(background_results)
     render json: BackgroundSerializer.new(background)
   end

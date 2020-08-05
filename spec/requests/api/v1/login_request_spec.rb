03e4cc4f61cf39d6583434eb8bcd3547bfe16fca
Antonio Jackson <antoniojackson@gmail.com>
2020-08-04 23:38:38 -0600

    Adding tests for no email address, incorrect password, and no password

--- spec/requests/api/v1/login_request_spec.rb
+++ spec/requests/api/v1/login_request_spec.rb
@@ -11,9 +11,7 @@ describe "Login Request API" do
 
   it "returns the user's api key for successful requests" do
     user_params = {"email": "whatever@example.com",
-                   "password": "password",
-                   "password_confirmation": "password"}
-
+                   "password": "password"}
     post "/api/v1/sessions", params: user_params
 
     expect(response.status).to eq(200)
@@ -34,12 +32,39 @@ describe "Login Request API" do
 
   it "returns a 400 level status code for unrecognized emails" do
     user_params = {"email": "whateve@example.com",
-                   "password": "password",
-                   "password_confirmation": "password"}
-
+                   "password": "password"}
     post "/api/v1/sessions", params: user_params
 
     expect(response.status).to eq(400)
     expect(response.body).to eq('The entered email address does not identify a user')
   end
+
+  it "returns a 400 level status code for empty email addresses" do
+    user_params = {"email": "",
+                   "password": "password"}
+    post "/api/v1/sessions", params: user_params
+
+    expect(response.status).to eq(400)
+    expect(response.body).to eq('The entered email address does not identify a user')
+  end
+
+
+    it "returns a 400 level status code for incorrect password" do
+      user_params = {"email": "whatever@example.com",
+                     "password": "passwor"}
+      post "/api/v1/sessions", params: user_params
+
+      expect(response.status).to eq(400)
+      expect(response.body).to eq('The entered password is incorrect for the entered email address')
+    end
+
+    it "returns a 400 level status code when no password is provided" do
+      user_params = {"email": "whatever@example.com",
+                     "password": ""}
+      post "/api/v1/sessions", params: user_params
+
+      expect(response.status).to eq(400)
+      expect(response.body).to eq('The entered password is incorrect for the entered email address')
+    end
+
 end

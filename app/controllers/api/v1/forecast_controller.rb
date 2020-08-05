b6a126565b41138795a5ee779c11aa94ab2aa1f8
Antonio Jackson <antoniojackson@gmail.com>
2020-08-05 09:03:36 -0600

    Adding RoadTripSerializer

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::ForecastController < ApplicationController
   def index
     location = forecast_params[:location]
     search_results = SearchResults.new(location)
-    coordinates = search_results.lat_long
+    coordinates = search_results.lat_long(location)
     forecast_results = search_results.open_weather_forecast(coordinates)
     forecast = Forecast.new(location, forecast_results)
     render json: ForecastSerializer.new(forecast)

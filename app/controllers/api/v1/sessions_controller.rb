0ce619f9de72a726cb6ae750595c6be8c54b71e5
Antonio Jackson <antoniojackson@gmail.com>
2020-08-04 23:26:17 -0600

    Updating sessions create action to return a 400 status for unrecognized email addresses

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,10 +1,13 @@
 class Api::V1::SessionsController < ApplicationController
 
   def create
-    user = User.find_by(email: user_params[:email])
-    if user.authenticate(user_params[:password])
+    unless user = User.find_by(email: user_params[:email])
+      render json: "The entered email address does not identify a user", status: 400
+    end
+    if user && user.authenticate(user_params[:password])
       session[:user_id] = user.id
       render json: UsersSerializer.new(user)
+    else
     end
   end
 

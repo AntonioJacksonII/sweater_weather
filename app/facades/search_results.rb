fecb2af0a0333bb458dc5da86de02bb5c6c9da64
Antonio Jackson <antoniojackson@gmail.com>
2020-08-04 18:49:07 -0600

    Adding weather_background_image method to SearchResults facade

--- app/facades/search_results.rb
+++ app/facades/search_results.rb
@@ -15,4 +15,10 @@ class SearchResults
   def open_weather_forecast(coordinates)
     OpenweatherService.new.get_forecast(coordinates)
   end
+
+  def weather_background_image
+    weather = OpenweatherService.new.get_forecast(lat_long)[:current][:weather].first[:main]
+    search_query = @location.split(",").first + " #{weather}"
+    UnsplashService.new.get_weather_image(search_query)
+  end
 end
